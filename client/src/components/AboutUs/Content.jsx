a00622a8e775e04e9990aa58269fb7e5bf27d155
HarshitKapse <harshit2001kapse@gmail.com>
2024-03-29 15:35:07 +0530

    Correct ion did

--- client/src/components/AboutUs/Content.jsx
+++ client/src/components/AboutUs/Content.jsx
@@ -11,12 +11,15 @@ const Content = () => {
                 <Text fontSize='4xl' ml='6.4%' mt={'3.4%'} mb='3.4%'>About Us</Text>
                 <Text as='b' color='#FF00FF' fontSize='3xl' ml='6.4%'>E-contractor</Text>
                 <Text ml='6.4%' mt='3.4%' w='40%' align={'justify'} fontSize={'lg'}>
+<<<<<<< Updated upstream
                     Econtractor is a Professional Platform. Here we will provide you only interesting content,
                     which you will like very much. We're dedicated to providing you the best of services,
                     with a focus on dependability and Find the information about people involved in construction of a building.
                     We're working to turn our passion for providing best we can into a booming online website.
                     We hope you enjoy our website as much as we enjoy offering them to you.
                     I will keep posting more important posts on my Website for all of you. Please give your support and love.
+=======
+>>>>>>> Stashed changes
                     
                 </Text>
                 <Button
